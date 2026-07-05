4b45f3991ec323590f3b5c714dab29971ba534d3
Onur Acar <onuracar.work@gmail.com>
2026-07-05 18:16:30 +0300

    feat(core): implement Signal primitive

---
+++ src/core/signal.ts
@@ -0,0 +1,40 @@
+import { activeEffect, ReactiveEffect } from './effect';
+
+export class Signal<T> {
+  private _value: T;
+  private subscribers: Set<ReactiveEffect> = new Set();
+
+  constructor(initialValue: T) {
+    this._value = initialValue;
+  }
+
+  get value(): T {
+    this.track();
+    return this._value;
+  }
+
+  set value(newValue: T) {
+    if (this._value !== newValue) {
+      this._value = newValue;
+      this.trigger();
+    }
+  }
+
+  private track() {
+    if (activeEffect) {
+      this.subscribers.add(activeEffect);
+      // Let the active effect know it is subscribed to this set
+      activeEffect.deps.add(this.subscribers);
+    }
+  }
+
+  private trigger() {
+    // Create a copy to prevent infinite loops if subscribers modify the set during iteration
+    const subs = new Set(this.subscribers);
+    subs.forEach((effect) => effect());
+  }
+}
+
+export function signal<T>(initialValue: T): Signal<T> {
+  return new Signal(initialValue);
+}
