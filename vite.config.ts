8a37bc804e451a833f6051eac8b9d5cef9216896
Onur Acar <onuracar.work@gmail.com>
2026-07-05 18:16:29 +0300

    chore: project initialization and configuration

---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import dts from 'vite-plugin-dts';
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'NimbleJS',
+      fileName: (format) => `nimblejs.${format}.js`
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: [],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {}
+      }
+    }
+  },
+  plugins: [dts({ rollupTypes: true })]
+});
